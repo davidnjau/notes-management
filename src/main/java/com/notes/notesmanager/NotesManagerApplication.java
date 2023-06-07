13470578b6d277a22a0786b0409bad7e0b0993ae
davidnjau <davidnjau21@gmail.com>
2023-06-07 05:43:38 +0300

    first commit

---
+++ src/main/java/com/notes/notesmanager/NotesManagerApplication.java
@@ -0,0 +1,13 @@
+package com.notes.notesmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotesManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/notes/notesmanager/NotesManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notes.notesmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
